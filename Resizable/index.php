f25f0a1cd29fb17628cb0469ac336f437e60c680
Duc Le <ductit1998@gmail.com>
2021-05-14 16:27:09 +0700

    Resizeable Shape

--- Resizable/index.php
+++ Resizable/index.php
@@ -3,12 +3,20 @@
 include_once "vendor/autoload.php";
 
 use Shape_3\Circle;
+use Shape_3\Rectangle;
+use Shape_3\Square;
 
-$circle = new Circle(5);
-$rectangle = new \Shape_3\Rectangle(5, 3);
-$square = new \Shape_3\Square(5);
-echo $circle . "<br>" . $rectangle . "<br>" . $square;
-$circle->resize(0.2);
-$rectangle->resize(0.2);
-$square->resize(0.2);
-echo "<br>".$circle . "<br>" . $rectangle . "<br>" . $square;
+//$circle = new Circle(5);
+//$rectangle = new Rectangle(5, 3);
+//$square = new Square(5);
+//echo $circle . "<br>" . $rectangle . "<br>" . $square;
+//$circle->resize(0.2);
+//$rectangle->resize(0.2);
+//$square->resize(0.2);
+//echo "<br>".$circle . "<br>" . $rectangle . "<br>" . $square;
+
+$shapes=[new Circle(5),new Rectangle(5,3),new Square(5)];
+foreach ($shapes as $shape){
+    $shape->resize(0.2);
+    echo $shape."<br>";
+}
